154d65ba3061d5819303d751dac98dd6e63d28fe
S-J-K123 <98359890+S-J-K123@users.noreply.github.com>
2024-01-24 14:39:20 +0000

    new

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import About from "./pages/About";
 import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
-import { projectsData } from "./projectsData";
-import Nav from "./components/Nav";
+import Nav from "./components/Nav"; // Import Nav component
 import { Link } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
-      <Link to="/about">About</Link>
-      <Link to="/projects">Projects</Link>
-      <Link to="/contact">Contact</Link>
-      <Routes>
-        {" "}
-        <Route path="/" exact component={About} />
-        <Route path="/about" component={About} />
-        <Route path="/contact" component={Contact} />{" "}
-      </Routes>{" "}
-    </Router>
+    <>
+      <Nav />
+      <Router>
+        <Routes>
+          <Route path="/" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 const Nav = () => {
   return (
-    <div>
+    <div className="nav-container">
       <nav>
         <figure>
           <img className="logo" src={logoImage} />
