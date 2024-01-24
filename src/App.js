ce9ccc142e8d3115d77e19a0b8b0cf21a8738050
S-J-K123 <98359890+S-J-K123@users.noreply.github.com>
2024-01-24 12:33:46 +0000

    new

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import About from "./pages/About"
-import Projects from './pages/Projects';
-import Contact from './pages/Contact';
-import { projectsData } from './projectsData';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Switch } from "react-router-dom";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+import { projectsData } from "./projectsData";
 import Nav from "./components/Nav";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-
-function App() {   return (     <Router>      <nav>        <ul>          <li>           
-   <Link to="/about">About</Link>          </li>          <li>            
-    <Link to="/projects">Projects</Link>          </li>          <li>           
-       <Link to="/contact">Contact</Link>          </li>        </ul>      </nav>      
-       <Switch>        <Route path="/" exact component={About} />       
-        <Route path="/about" component={About} />        
-        <Route path="/contact" component={Contact} />      </Switch>    </Router>  ); }
+function App() {
+  return (
+    <Router>
+      <Link to="/about">About</Link>
+      <Link to="/projects">Projects</Link>
+      <Link to="/contact">Contact</Link>
+      <Routes>
+        {" "}
+        <Route path="/" exact component={About} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />{" "}
+      </Routes>{" "}
+    </Router>
+  );
+}
 
 export default App;
